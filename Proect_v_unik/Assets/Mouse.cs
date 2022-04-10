0268a058b3288c357a74cabc6417a528859f6353
CumTZN <artem.nazarov.0208@mail.ru>
2022-04-11 01:30:59 +0300

    Jivoi_poka
    Поле

---
+++ Proect_v_unik/Assets/Mouse.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour{
+
+    public Material Red;
+    public Material Blue;
+    Material m_Material;
+
+    void Start(){
+        m_Material = GetComponent<Renderer> ().material;
+    }
+
+    private void OnMouseOver(){
+        m_Material.color=Red.color;
+    }
+
+    private void OnMouseExit(){ 
+        m_Material.color = Blue.color;
+    }
+
+}
